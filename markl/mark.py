13d6784d3f5c2319435df6db0648bd322eafeeb0
Юрий Сагитов <yu.sagitov@2gis.ru>
2022-11-01 15:26:56 +0300

    параметризировал название аттрибута и учитыываем переносы после тега

--- markl/mark.py
+++ markl/mark.py
@@ -14,8 +14,10 @@ def mark(dir='.'):
             idx = 0
             while (idx := file_body.find('<' + tag, idx + 1)) != -1:
                 uuid = uuid4()
-                if ' mark' != file_body[idx + 1 + len(tag):idx + 1 + len(tag) + 5] and ' ' == file_body[idx + 1 + len(tag):idx + 1 + len(tag) + 1]:
-                    file_body = file_body[:idx + 1 + len(tag)] + f' mark="{uuid}" ' + file_body[idx + 1 + len(tag):]
+                next_word = file_body[idx + 1 + len(tag):idx + 1 + len(tag) + 5]
+                next_symbol = file_body[idx + 1 + len(tag):idx + 1 + len(tag) + 1]
+                if next_word != f' {Config.ATTR}' and (next_symbol == ' ' or next_symbol == '\n'):
+                    file_body = file_body[:idx + 1 + len(tag)] + f' {Config.ATTR}="{uuid}" ' + file_body[idx + 1 + len(tag):]
 
         # Write the file out again
         with open(file_name, 'w') as file:
